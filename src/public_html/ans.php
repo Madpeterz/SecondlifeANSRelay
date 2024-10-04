62f596a4a320f54ab6cabba11e95e4b1d6992af2
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2024-10-05 00:07:18 +0100

    Update ans.php

--- src/public_html/ans.php
+++ src/public_html/ans.php
@@ -23,36 +23,36 @@ foreach (getenv() as $key => $value) {
 
 if (count($relayTargets) == 0) {
     http_response_code(501);
-    error_log('no relay targets loaded', 3, 'php://stdout');
+    error_log('no relay targets loaded', 3, '/proc/self/fd/2');
     die("Not accepted here"); // no relays are setup
 }
 
 if ($ansSalt == "notLoaded") {
     http_response_code(501);
-    error_log('ANS salt not loaded', 3, 'php://stdout');
+    error_log('ANS salt not loaded', 3, '/proc/self/fd/2');
     die("Not accepted here"); // ENV for ANS salt not setup
 }
 
 if (array_key_exists("HTTP_X_ANS_VERIFY_HASH", $_SERVER) == false) {
     http_response_code(501);
-    error_log('HTTP_X_ANS_VERIFY_HASH is missing', 3, 'php://stdout');
+    error_log('HTTP_X_ANS_VERIFY_HASH is missing', 3, '/proc/self/fd/2');
     die("Not accepted here"); // HTTP_X_ANS_VERIFY_HASH is missing
 }
 $checkHash = $_SERVER['HTTP_X_ANS_VERIFY_HASH'];
 if ($checkHash == null) {
     http_response_code(501);
-    error_log('HTTP_X_ANS_VERIFY_HASH is empty', 3, 'php://stdout');
+    error_log('HTTP_X_ANS_VERIFY_HASH is empty', 3, '/proc/self/fd/2');
     die("Not accepted here"); // HTTP_X_ANS_VERIFY_HASH is empty
 }
 if (array_key_exists("QUERY_STRING", $_SERVER) == false) {
     http_response_code(501);
-    error_log('QUERY_STRING is missing', 3, 'php://stdout');
+    error_log('QUERY_STRING is missing', 3, '/proc/self/fd/2');
     die("Not accepted here"); // QUERY_STRING is missing
 }
 $vaildateHash = sha1($_SERVER['QUERY_STRING'] . $ansSalt);
 if ($checkHash != $vaildateHash) {
     http_response_code(501);
-    error_log('hash checks failed', 3, 'php://stdout');
+    error_log('hash checks failed', 3, '/proc/self/fd/2');
     die("Not accepted here"); // did not pass checks on the source
 }
 
@@ -67,12 +67,12 @@ $client = new Client(['handler' => $handle]);
 $headers = ['HTTP_X_ANS_VERIFY_HASH' => $_SERVER['HTTP_X_ANS_VERIFY_HASH']];
 foreach ($relayTargets as $relay) {
     try {
-        error_log("sending to " . $url, 3, 'php://stdout');
+        error_log("sending to " . $url, 3, '/proc/self/fd/2');
         $relayUri = $url . "?" . $_SERVER['QUERY_STRING'];
         $request = new Request("GET", $relayUri, $headers, "");
         $promises[] = $client->getAsync($request);
     } catch (Exception $e) {
-        error_log("Failed to write to a relay target " . $relay . " because: " . $e->getMessage(), 3, 'php://stdout');
+        error_log("Failed to write to a relay target " . $relay . " because: " . $e->getMessage(), 3, '/proc/self/fd/2');
     }
 }
 
@@ -88,7 +88,7 @@ $last = microtime(true);
 while ($done < $waiting) {
     $now = microtime(true);
     $delta = round(($now - $last) * 1000);
-    error_log("tick(" . $delta . ")", 3, 'php://stdout');
+    error_log("tick(" . $delta . ")", 3, '/proc/self/fd/2');
     $last = $now;
     $handler->tick();
 }
