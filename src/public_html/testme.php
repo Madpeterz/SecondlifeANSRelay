392b7844a63e013b70fcb8cf8161b4b0dccf1aff
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2024-10-05 23:21:31 +0100

    Update testme.php

--- src/public_html/testme.php
+++ src/public_html/testme.php
@@ -7,6 +7,7 @@ file_put_contents('php://stdout', 'stdout test');
 file_put_contents('php://stderr', 'stderr test');
 file_put_contents('/proc/self/fd/2', 'fd/2 test');
 file_put_contents('/proc/self/fd/1', 'fd/1 test');
+file_put_contents('/proc/1/fd/1', '1/fd/1 test');
 file_put_contents('/dev/null', 'dev/null test');
 file_put_contents('/dev/stderr', 'dev/stderr test');
 file_put_contents('/dev/stdout', 'dev/stdout test');
