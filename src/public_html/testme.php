7c8d7a9779d265a2dfbc7585d4f60efbc6c85133
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2024-10-05 23:34:45 +0100

    Update testme.php

--- src/public_html/testme.php
+++ src/public_html/testme.php
@@ -2,12 +2,19 @@
 
 @ini_set('display_errors', "off");
 @ini_set('log_errors', "on");
-@ini_set('error_log', '/proc/self/fd/2');
-file_put_contents('php://stdout', 'stdout test');
-file_put_contents('php://stderr', 'stderr test');
-file_put_contents('/proc/self/fd/2', 'fd/2 test');
-file_put_contents('/proc/self/fd/1', 'fd/1 test');
-file_put_contents('/proc/1/fd/1', '1/fd/1 test');
-file_put_contents('/dev/null', 'dev/null test');
-file_put_contents('/dev/stderr', 'dev/stderr test');
-file_put_contents('/dev/stdout', 'dev/stdout test');
+
+$points = [
+    'php://stdout',
+    'php://stderr',
+    '/proc/self/fd/2',
+    '/proc/self/fd/1',
+    '/proc/1/fd/1',
+    '/dev/null',
+    '/dev/stderr',
+    '/dev/stdout',
+];
+
+foreach ($points as $p) {
+    @ini_set('error_log', $p);
+    error_log("test of :" . $p);
+}
